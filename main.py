6e8f2c686b87f8f74695effee8f4f4c91da0c8bb
devinrgit <devin.git.mail@gmail.com>
2024-10-01 15:42:04 -0400

    added report to main function to output book title, word count, and letter count

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-def main(book):
-    with open(book) as f:
-        file_contents = f.read()
-    print(file_contents)
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    char_dict = char_count(text)
+    print(f"---Begin report of", book_path, "---")
+    print(num_words, "words found in the document")
+    for c in char_dict:
+        print(f"The '", c , "' character was found ", char_dict[c], "times")
 
-def word_count(book):
-    with open(book) as f:
-        contents = f.read()
-        words = contents.split()
-        
-        return len(words)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+def word_count(text):
+    words = text.split()
+    return len(words)
 
-def char_count(book):
-    with open(book) as f:
-        contents = f.read().lower()
-        chars = {}
-        for char in contents:
-            if char.isalpha():
-                if char not in chars:
-                    chars[char] = 1
-                else:
-                    chars[char] += 1
-        return chars
-        
-def report(book):
-    print(f"---Begin report of", book, "---")
-    print(word_count(book), "words found in the document")
+def char_count(text):
+    chars = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered.isalpha():
+            if lowered not in chars:
+                chars[lowered] = 1
+            else:
+                chars[lowered] += 1
+    return chars
 
-
+main()
 
